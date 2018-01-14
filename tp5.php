7dc5fa7ba19dce93e1c96f33d3fdd9071ab45bed
Abbaoui <abbaouiamine.r@gmail.com>
2018-01-14 14:00:32 -0800

    Add files via upload

---
+++ tp5.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+    ============Etape 1 : 
+    $pdo=new PDO("mysql:host=xxx;dbname=xxx","","");
+    //chaine 1 : 
+    mysql:host=localhost;dbname=nomBase
+    //chaine 2 :
+    nomuser
+    //chaine 3 :
+    pass
+    ============Etape2 :
+    $stm=$pdo->prepare("req");
+    ============Etape 3:
+
+    $stm->execute();
+*/
+
+
+
+?>
